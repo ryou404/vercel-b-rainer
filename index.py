ec310e40c653484849b8f24950fe436d243dc830
ryou404 <help36611776@gmail.com>
2025-03-27 10:52:45 +0800

    Update index.py

--- index.py
+++ index.py
@@ -1,50 +1,36 @@
 from flask import Flask, render_template, request
-from datetime import datetime,timezone,timedelta
+from datetime import datetime, timezone, timedelta
 
 app = Flask(__name__)
 
 @app.route("/account", methods=["GET", "POST"])
 def account():
     if request.method == "POST":
-        user = request.form["user"]
-        pwd = request.form["pwd"]
-        result = "您輸入的帳號是：" + user + "; 密碼為：" + pwd 
+        user = request.form.get("user", "未提供")  
+        pwd = request.form.get("pwd", "未提供")  
+        result = f"您輸入的帳號是：{user}; 密碼為：{pwd}"
         return result
-    else:
-        return render_template("account.html")
-
-
+    return render_template("account.html")
 
 @app.route("/about")
 def me():
     return render_template("about.html")
 
-
-
-
-
-
 @app.route("/welcome", methods=["GET", "POST"])
 def welcome():
     user = request.values.get("nick")  
-     w = request.values.get("work")
-    return render_template("welcome.html", name=user) 
+    w = request.values.get("work")  # 修正縮排錯誤
+    return render_template("welcome.html", name=user, work=w)  # 傳遞 work 變數
 
 @app.route("/")
 def index():
-    homepage = "<h1>郭碩元Python網頁</h1>"
+    homepage = "<h1>郭碩元 Python 網頁</h1>"
     homepage += "<a href=/mis>MIS</a><br>"
     homepage += "<a href=/today>顯示日期時間</a><br>"
-    homepage += "<a href=/welcome?nick=tcyang>傳送使用者暱稱</a><br>"
+    homepage += "<a href=/welcome?nick=tcyang&work=developer>傳送使用者暱稱與工作</a><br>"
     homepage += "<a href=/account>網頁表單傳值</a><br>"
     homepage += "<a href=/about>郭碩元簡介網頁</a><br>"
-    return homepage
-
-
-
-
-
-    return "hello 郭碩元!"
+    return homepage  # 刪除多餘的 return
 
 @app.route("/mis")
 def course():
@@ -57,4 +43,4 @@ def today():
     return render_template("today.html", datetime=str(now))
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)  # 啟用 debug 模式
